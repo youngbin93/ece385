bc86e07263a1f05e440e798abb0c4974255afeae
Faramola Isiaka <fisiaka2@illinois.edu>
2017-09-18 14:45:35 -0500

    clean up

--- lab4/bit_serial_processor/simulation/modelsim/rtl_work/sync/_primary.vhd
+++
@@ -1,9 +0,0 @@
-library verilog;
-use verilog.vl_types.all;
-entity sync is
-    port(
-        Clk             : in     vl_logic;
-        d               : in     vl_logic;
-        q               : out    vl_logic
-    );
-end sync;
