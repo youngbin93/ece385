556f198d6f178ef09ff092c3e6bf88e9f6e908c6
Isiaka <fisiaka2@illinois.edu>
2017-09-19 10:06:39 -0500

    cra compiles might need to modify more

--- lab4/adders/ripple_adder/lab4_adders_toplevel.sv
+++ lab4/adders/ripple_adder/lab4_adders_toplevel.sv
@@ -36,7 +36,8 @@ module lab4_adders_toplevel
 );
 
     /* Declare Internal Registers */
-    logic[15:0]     R; 
+    logic[15:0]     A;  // use this as an input to your adder
+    logic[15:0]     B;  // use this as an input to your adder
     
     /* Declare Internal Wires
      * Wheather an internal logic signal becomes a register or wire depends
@@ -57,19 +58,22 @@ module lab4_adders_toplevel
         
         if (!Reset) begin
             // if reset is pressed, clear the adder's input registers
-            R <= 16'h0000;
+            A <= 16'h0000;
+            B <= 16'h0000;
             Sum <= 16'h0000;
             CO <= 1'b0;
         end else if (!LoadB) begin
             // If LoadB is pressed, copy switches to register B
-            R <= SW;
-        end 
+            B <= SW;
+        end else begin
+            // otherwise, continuously copy switches to register A
+            A <= SW;
+        end
         
         // transfer sum and carry-out from adder to output register
         // every clock cycle that Run is pressed
         if (!Run) begin
             Sum <= Sum_comb;
-				R <= Sum_comb;
             CO <= CO_comb;
         end
             // else, Sum and CO maintain their previous values
@@ -103,58 +107,74 @@ module lab4_adders_toplevel
 	  
 	 ripple_adder ripple_adder_inst
     (
-        .A(SW[15:0]),             // This is shorthand for .A(A) when both wires/registers have the same name
-        .B(R[15:0]),
+        .A,             // This is shorthand for .A(A) when both wires/registers have the same name
+        .B,
         .Sum(Sum_comb), // Connects the Sum_comb wire in this file to the Sum wire in ripple_adder.sv
         .CO(CO_comb)
     );
-	     
+	 
+//    carry_lookahead_adder carry_lookahead_adder_inst
+//    (
+//        .A,             // This is shorthand for .A(A) when both wires/registers have the same name
+//        .B,
+//        .Sum(Sum_comb), // Connects the Sum_comb wire in this file to the Sum wire in ripple_adder.sv
+//        .CO(CO_comb)
+//    );
+
+//    carry_select_adder carry_select_adder_inst
+//    (
+//        .A,             // This is shorthand for .A(A) when both wires/registers have the same name
+//        .B,
+//        .Sum(Sum_comb), // Connects the Sum_comb wire in this file to the Sum wire in ripple_adder.sv
+//        .CO(CO_comb)
+//    );
+    
     HexDriver Ahex0_inst
     (
-        .In0(SW[3:0]),   // This connects the 4 least significant bits of 
+        .In0(A[3:0]),   // This connects the 4 least significant bits of 
                         // register A to the input of a hex driver named Ahex0_inst
         .Out0(Ahex0_comb)
     );
     
     HexDriver Ahex1_inst
     (
-        .In0(SW[7:4]),
+        .In0(A[7:4]),
         .Out0(Ahex1_comb)
     );
     
     HexDriver Ahex2_inst
     (
-        .In0(SW[11:8]),
+        .In0(A[11:8]),
         .Out0(Ahex2_comb)
     );
     
     HexDriver Ahex3_inst
     (
-        .In0(SW[15:12]),
+        .In0(A[15:12]),
         .Out0(Ahex3_comb)
     );
     
     HexDriver Bhex0_inst
     (
-        .In0(R[3:0]),
+        .In0(B[3:0]),
         .Out0(Bhex0_comb)
     );
     
     HexDriver Bhex1_inst
     (
-        .In0(R[7:4]),
+        .In0(B[7:4]),
         .Out0(Bhex1_comb)
     );
     
     HexDriver Bhex2_inst
     (
-        .In0(R[11:8]),
+        .In0(B[11:8]),
         .Out0(Bhex2_comb)
     );
     
     HexDriver Bhex3_inst
     (
-        .In0(R[15:12]),
+        .In0(B[15:12]),
         .Out0(Bhex3_comb)
     );
     
--- lab4/adders/ripple_adder/ripple_adder.sv
+++ lab4/adders/ripple_adder/ripple_adder.sv
@@ -14,9 +14,9 @@ module ripple_adder
 	  // Internal carries the 2-bit adder
 		wire c1, c2, c3;
 		
-		four_bit_cra CR0(.x(A[3:0]),   .y(B[3:0]),  .z(0),   .s(Sum[3:0]),  .c(c1));
-		four_bit_cra CR1(.x(A[7:4]),   .y(B[7:4]),  .z(c1),  .s(Sum[7:4]),  .c(c2));
-		four_bit_cra CR2(.x(A[11:8]),   .y(B[11:8]),  .z(c2),  .s(Sum[11:8]),  .c(c3));
-		four_bit_cra CR3(.x(A[15:12]),   .y(B[15:12]),  .z(c3),  .s(Sum[15:12]),  .c(CO));
+		four_bit_cra FCR0(.x(A[3:0]),   .y(B[3:0]),  .z(0),   .s(Sum[3:0]),  .c(c1));
+		four_bit_cra FCR1(.x(A[7:4]),   .y(B[7:4]),  .z(c1),  .s(Sum[7:4]),  .c(c2));
+		four_bit_cra FCR2(.x(A[11:8]),   .y(B[11:8]),  .z(c2),  .s(Sum[11:8]),  .c(c3));
+		four_bit_cra FCR3(.x(A[15:12]),   .y(B[15:12]),  .z(c3),  .s(Sum[15:12]),  .c(CO));
 
 endmodule
