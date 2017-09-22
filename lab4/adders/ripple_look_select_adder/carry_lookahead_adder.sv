e02330ef6ced7c53fa60eb4a8655809980cb53ba
Faramola Isiaka <fisiaka2@illinois.edu>
2017-09-22 18:14:15 -0500

    patch a mistake

--- lab4/adders/ripple_look_select_adder/carry_lookahead_adder.sv
+++ lab4/adders/ripple_look_select_adder/carry_lookahead_adder.sv
@@ -13,13 +13,13 @@ module carry_lookahead_adder
      * Feel free to create sub-modules or other files. */
 	
 	
-		logic c1, c2, c3;
+		logic c1, c2, c3, c4;
 		logic [3:0] p, g;
 		
 		four_bit_cla CLA0(.a(A[3:0]),   .b(B[3:0]),   .s(Sum[3:0]),  .c_in(0),  .c_out(),  .p_out(p[0]),  .g_out(g[0]));
 		four_bit_cla CLA1(.a(A[7:4]),   .b(B[7:4]),   .s(Sum[7:4]),  .c_in(c1), .c_out(),  .p_out(p[1]),  .g_out(g[1]));
 		four_bit_cla CLA2(.a(A[11:8]),  .b(B[11:8]),  .s(Sum[11:8]), .c_in(c2), .c_out(),  .p_out(p[2]),  .g_out(g[2]));
-		four_bit_cla CLA3(.a(A[15:12]), .b(B[15:12]), .s(Sum[15:12]),.c_in(c3), .c_out(CO),  .p_out(p[3]),  .g_out(g[3]));
+		four_bit_cla CLA3(.a(A[15:12]), .b(B[15:12]), .s(Sum[15:12]),.c_in(c3), .c_out(),  .p_out(p[3]),  .g_out(g[3]));
 		
 		always_comb
 		begin 
