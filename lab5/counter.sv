366d5b9593b0393f3c672533e7aef5f03a9e1e76
Hasan Alatrakchi <alattra2@dcl-l440-12.ews.illinois.edu>
2017-09-23 15:11:15 -0500

    modified counter

--- lab5/counter.sv
+++ lab5/counter.sv
@@ -2,18 +2,18 @@ module counter(
 			input logic  down_up,  
 			input logic clk,  
 			input logic reset,   
-			output reg  [7:0] out, 
+			output reg  [3:0] out, 
 
 		);
   
 
 	always_ff @(posedge clk)
-  if (reset) begin // active high reset
-    out <= 8'b0 ;
-  end else if (up_down) begin
-    out ++;
-  end else begin
-    out --;
-  end
-  
+	begin
+		  if (reset)  
+			 out <= 4'b0;
+		  else 
+			 out ++;
+
+	end
+		  
   endmodule
