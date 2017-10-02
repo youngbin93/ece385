2b52dfc802820b31e86027ded798fdc3c8521b56
Faramola Isiaka <faramolaisiaka@yahoo.com>
2017-10-01 20:11:17 -0500

    add the tests for all signs for lab simulations

--- lab5/testbench.sv
+++ lab5/testbench.sv
@@ -34,39 +34,99 @@ eight_bit_multiplier tp(.*);
 
 initial begin : TEST_VECTORS
 
-		/* Load B with FF */
-	#2 SW = 8'b11111111;
-		Reset = 0;
+		/* Reset */
+	#2 	Reset = 0;
 		Run = 1 ;
 		ClearA_LoadB = 1;
 		
 		/* blank */
-	#2 SW = 8'b11111111;
-		Reset = 1;
-		Run = 1 ;
-		ClearA_LoadB = 1;
+	#2 	Reset = 1;
 		
 		/* Load B with FF */
-	#2 SW = 8'b11111111;
-		Reset = 1;
-		Run = 1 ;
+	#2 	SW = 8'b11111111;
 		ClearA_LoadB = 0;
 		
 		/* Blank */
-	#2 SW = 8'b11111111;
-		Reset = 1;
-		ClearA_LoadB = 1;
-		Run = 1;
+	#2 	ClearA_LoadB = 1;
 		
 		/* B is FF and multiply by FF */
-	#2 SW = 8'b11111111;
-		Reset = 1;
-		ClearA_LoadB = 1;
-		Run = 0;
+	#2 	Run = 0;
 		
 		/* Release Run */
-	#2 Run = 1;
+	#2 	Run = 1;
 	
+	/* Takes about 16 cycles to finish */
+		/* 1 * -1 */
+	
+ 		/* Reset */
+	#17 	Reset = 0;
+	
+		/* blank */
+	#2 	Reset = 1;
+	
+		/* Load B with FF */
+	#2 	SW = 8'b11111111;
+		ClearA_LoadB = 0;
+	
+		/* blank */
+	#2 	ClearA_LoadB = 1;
+	
+		/* Set Switches to 01 */
+	#2 	SW = 8'b00000001;
+	
+		/* Press Run */
+	#2 	Run = 0 ;
+		
+		/*Release Run*/
+	#2 	Run = 1 ;
+	
+	/* Takes about 16 cycles to finish */
+	/* 1 * 1 */
+ 		/* Reset */
+	#17 	Reset = 0;
+	
+		/* blank */
+	#2 	Reset = 1;
+	
+		/* Load B with 01 */
+	#2 	SW = 8'b00000001;
+		ClearA_LoadB = 0;
+	
+		/* blank */
+	#2 	ClearA_LoadB = 1;
+	
+		/* Set Switches to 01 */
+	#2 	SW = 8'b00000001;
+	
+		/* Press Run */
+	#2 	Run = 0 ;
+		
+		/*Release Run*/
+	#2 	Run = 1 ;
+	
+	/* Takes about 16 cycles to finish */
+	/* -1 * 1 */
+ 		/* Reset */
+	#17 	Reset = 0;
+	
+		/* blank */
+	#2 	Reset = 1;
+	
+		/* Load B with 01 */
+	#2 	SW = 8'b00000001;
+		ClearA_LoadB = 0;
+	
+		/* blank */
+	#2 	ClearA_LoadB = 1;
+	
+		/* Set Switches to FF */
+	#2 	SW = 8'b11111111;
+	
+		/* Press Run */
+	#2 	Run = 0 ;
+		
+		/*Release Run*/
+	#2 	Run = 1 ;
+			
 end
-
 endmodule
