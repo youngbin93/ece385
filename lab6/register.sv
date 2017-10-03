e56efe20fce63a3c4e0a01593f8bea7517f29fa2
Faramola Isiaka <fisiaka2@illinois.edu>
2017-10-03 10:37:20 -0500

    parameterizable register

---
+++ lab6/register.sv
@@ -0,0 +1,24 @@
+module register #(parameter size = 16)
+(
+	input logic Clk, Load
+	input logic [size - 1:0] in,
+	output logic [size - 1:0] out
+);
+
+logic [size - 1:0] data; 
+
+/* Registers start with zero in them */
+initial 
+begin 
+	data = 1'b0;
+end 
+
+always_ff @(posedge Clk)
+begin
+	if(Load)
+	begin
+		data = in;
+	end
+end
+
+assign out = data;
